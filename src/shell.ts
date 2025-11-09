ee7b1faed105b05561f1561bff4608015a409b4b
Aatrox <guohaonan980421@163.com>
2025-11-09 16:25:51 +0800

    feat: syslink

--- src/shell.ts
+++ src/shell.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import shell from 'shelljs';
+shell.config.execPath = process.execPath;
 
 // docs: https://github.com/ggerganov/whisper.cpp
 const WHISPER_CPP_PATH = path.join(__dirname, '..', 'lib/whisper.cpp');
